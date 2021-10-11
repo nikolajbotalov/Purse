06feb0c156037b49425d3c9cad087e2fa14c3eeb
nikolajbotalov <nikolaj.botalov@gmail.com>
2021-10-11 19:13:21 +0500

    add node, express and mongo

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as BalanceBlock } from './BalanceBlock';
-export { default as BalanceInput } from './BalanceInput';
-export { default as BalanceItem } from './BalanceItem';
-export { default as Button } from './Button';
-export { default as Description } from './Description';
-export { default as Header } from './Header';
-export { default as HomeHeader } from './HomeHeader';
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as BalancePage } from './BalancePage';
-export { default as EditSourceBalancePage } from './EditSourceBalancePage';
-export { default as HomePage } from './HomePage';
-export { default as NewSourceBalancePage } from './NewSourceBalancePage';
-export { default as ChangeBalancePage } from './ChangeBalancePage';
